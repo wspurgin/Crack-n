577fa8d332f2a53dfb4e886ca4394c7f3be41926
Nchaos <Chaosnick@hotmail.com>
2014-10-23 16:41:42 -0700

    ProjectJS2

--- packages/custom/project/server/models/project.js
+++ packages/custom/project/server/models/project.js
@@ -12,7 +12,38 @@ Schema = mongoose.Schema;
 * Project Schema
 */
 
-var projectSchema = new Schema({});
+var projectSchema = new Schema({
+
+	name: {
+		type: String,
+		required: true
+	},
+	startDate: {
+		type: Date,
+		default: Date.now,
+		required: true
+	},
+	endDate: {
+		type: Date,
+		required: false
+	},
+	phases: {
+		type: Array
+	},
+	admin: {
+		type: Schema.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	members: {
+		type: Array
+	},
+	messeges: { 
+		type: Array
+	}
+});
 		 
 
 mongoose.model('Project', projectSchema);
+
+
