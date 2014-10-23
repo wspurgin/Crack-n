08c9eada1086a5665beaf8ef9888f9e899feb037
aacarroll <aacarroll@smu.edu>
2014-10-23 10:28:12 -0500

    Added model for tasks

---
+++ packages/custom/project/server/models/task.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+* Module dependencies
+*/
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+/**
+* Tasks Schema
+*/
+
+var taskSchema = new Schema ({
+	name: {
+		type: String,
+		required: true
+	},
+	completed: {
+		type: Boolean,
+		required: true
+	},
+	description: {
+		type: String, 
+		required: false
+	},
+	assignedMembers: {
+		type: Array,
+		required: true
+	},
+	dueDates: {
+		type: Date,
+		required: true
+	}
+});
+
+mongoose.model('Tasks',taskSchema);
