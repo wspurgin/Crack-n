3589aff2cbb72a83f8d76c2e872fb9991423137c
Aloqla <aaloqla@smu.edu>
2014-10-23 17:24:49 -0700

    header structure

--- packages/system/public/controllers/header.js
+++ packages/system/public/controllers/header.js
@@ -35,4 +35,47 @@ angular.module('mean.system').controller('HeaderController', ['$scope', '$rootSc
     });
 
   }
+])
+.controller('LoginController', ['$scope', '$rootScope', '$http', '$location', 'Global',
+  function($scope, $rootScope, $http, $location, Global) {
+    // This object will be filled by the form
+    $scope.user = {};
+    $scope.global = Global;
+    $scope.global.registerForm = false;
+    $scope.input = {
+      type: 'password',
+      placeholder: 'Password',
+      confirmPlaceholder: 'Repeat Password',
+      iconClass: '',
+      tooltipText: 'Show password'
+    };
+
+    // Register the login() function
+    $scope.login = function() {
+      $http.post('/login', {
+        email: $scope.user.email,
+        password: $scope.user.password
+      })
+        .success(function(response) {
+          // authentication OK
+          $scope.loginError = 0;
+          $rootScope.user = response.user;
+          $rootScope.$emit('loggedin');
+          if (response.redirect) {
+            if (window.location.href === response.redirect) {
+              //This is so an admin user will get full admin page
+              window.location.reload();
+            } else {
+              window.location = response.redirect;
+            }
+          } else {
+            window.location.reload();
+          }
+        })
+        .error(function() {
+          $scope.loginerror = 'Authentication failed.';
+        });
+    };
+  }
 ]);
+
