8ebe7e8bede1672e8bedf6d3f8f184da380abdf6
William O'Connor <woconnor@smu.edu>
2014-10-23 19:28:08 -0500

    changes phase route

--- packages/custom/project/server/routes/phase.js
+++ packages/custom/project/server/routes/phase.js
@@ -7,13 +7,12 @@ module.exports = function(Phase, app, auth, database, passport) {
   app.route('/projects/:project_id/phases')
     .get(phase.all);
 
+  app.route('/projects/:project_id/phases')
+  	.post(phase.addPhase);
+
   app.route('/projects/:project_id/phases/:phase_id')
     .get(phase.show);
 
   app.route('/projects/:project_id/phases/:phase_id')
   	.put(phase.edit);
-
-  app.route('/projects/:project_id/new-phase')
-  	.post(phase.addPhase);
-   
 };
