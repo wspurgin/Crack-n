2327d5facb70514a32b871c9c542cf26293b18a0
William O'Connor <woconnor@smu.edu>
2014-10-30 22:26:59 -0500

    change in project ctrl

--- packages/custom/project/server/controllers/project.js
+++ packages/custom/project/server/controllers/project.js
@@ -54,7 +54,7 @@ exports.addProject = function (req, res) {
     if (req.body.startDate) project.startDate = req.body.startDate;
     project.endDate = req.body.endDate;
     project.phases = [];
-    project.admin = req.body.user;
+    project.admin = req.body.admin;
     project.members = [];
     project.messages = [];
     project.save();
