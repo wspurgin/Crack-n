f562271b3e4cb745fd33bd4fee2d247d5f6a90e1
aacarroll <aacarroll@smu.edu>
2014-11-01 13:13:03 -0500

    Added a .show controller, as well as fixing things. Also (potentially) consolidating complete and assign controllers with edit

--- packages/custom/project/server/controllers/task.js
+++ packages/custom/project/server/controllers/task.js
@@ -32,19 +32,39 @@ exports.all=function(req,res) {
 
 
 //assign task to member
+/*
 exports.assign=function (req, res) {
 	var project_id = req.params.project_id;
+	var phase_id = req.params.phase_id;
+	var task_id = req.params.task_id; 
 	Project.findOne({'_id':project_id}).exec(function(err, result) {
 		if (!err) {
-			return res.json(200, result.task);
+			Phase.findOne({'_id':phase_id}).exec(function(err, result_phase){
+				if (!err) {
+					Task.findOne({'_id':task_id}).exec(function(err, result_task) {
+						if (!err){
+							task.assignedMembers=req.body.assignedMembers;
+							task.save();
+							result_task.assignedMembers.push(task.assignedMembers);
+							result_task.save();
+							return res.json(201, 'Member(s) successfully assigned!');
+						}
+						else {
+							return res.json('Task does not exist!');
+						}
+					});
+				}
+				else {
+					return res.json('Phase does not exist!');
+				}
+			});
 		}
 		else {
-			return res.json('Project does not exist!');
+			return res.json('Project does not exist!')
 		}
-		console.log('project' + result.task);
 	});
 };
-
+*/
 exports.complete=function(req, res) {
 	var project_id=req.params.project_id;
 	var task_id=req.params.task_id;
@@ -114,7 +134,7 @@ exports.edit=function(req, res) {
 					if (req.body.assignedMembers) result_task.assignedMembers=req.body.assignedMembers;
 					if (req.body.dueDates) result_task.dueDates=req.body.dueDates;
 					result_task.save();
-					return res.json(200, 'Successfully updated task!');
+					return res.json(200, 'Successfully edited task!', result_task);
 				}
 				else {
 					return res.json('Task does not exist!');
@@ -127,6 +147,34 @@ exports.edit=function(req, res) {
 	});
 };
 
+exports.show=function(req,res) {
+	var project_id=req.params.project_id;
+	var task_id=req.params.task_id;
+	var phase_id=req.params.phase_id;
+	Project.findOne({'_id': project_id}).exec(function(err, result) {
+		if (!err && result) {
+			Phase.findOne( {'_id': phase_id}).exec(function(err, result_phase) {
+				if (!err && result_phase) {
+					Task.findOne( {'_id':task_id}).exec(function(err,result_task) {
+						if (!err && result_task) {
+							return res.json(200, result_task);
+						}
+						else {
+							return ('Task does not exist!');
+						}
+					}); 
+				}
+				else {
+					return ('Phase does not exist!');
+				}
+			});			
+		}
+		else {
+			return ('Project does not exist!');
+		}
+	});
+}
+
 exports.delete=function(req,res) {
 	var project_id=req.params.project_id;
 	var task_id=req.params.task_id;
@@ -134,7 +182,7 @@ exports.delete=function(req,res) {
 		if (!err) {
 			Task.findOne( {'_id':task_id}).exec(function(err, result_task) {
 				if (!err) {
-					result_task.delete();
+					result_task.remove();
 					return res.json (200, 'Successfully deleted task!');
 				}
 				else {
