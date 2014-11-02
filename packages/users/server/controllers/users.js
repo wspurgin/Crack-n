0f986e7e346e511f4790bad21f0440135edb4cf1
Nigel Gilbert <ngilbert@smu.edu>
2014-11-01 22:29:31 -0500

    Accidentally deleted searchUsers function, fixed

--- packages/users/server/controllers/users.js
+++ packages/users/server/controllers/users.js
@@ -320,3 +320,47 @@ exports.forgotpassword = function(req, res, next) {
     }
   );
 };
+
+/*
+*searchUser function (takes a substring and callback function)
+*/
+exports.searchUsers = function(substring, cb) {
+  var exp = new RegExp('^' + substring + '.*', 'i');
+  async.waterfall([
+    // Make query
+    function(callback) {
+      User
+        .find()
+        .or([
+          {'username' : {$regex: exp}},
+          {'email '   : {$regex: exp}},
+        ])
+        .lean()
+        .select('username email name')
+        .sort('_id')
+        .limit(1000)
+        .exec(function(err, query) {
+          if (err) callback(err, query);
+          callback(null, query);
+        });
+      },
+    // Remove duplicates (O(n), figured it was fine)
+    function(users, callback) {
+      var cur = users[0]._id.toString();
+      var cleanedQuery = [];
+      cleanedQuery.push(users[0]);
+      for (var i = 1; i < users.length; i+=1) {
+        /*jslint eqeq: true*/
+        if (cur != users[i]._id.toString()) {
+          cur = users[i]._id.toString();
+          cleanedQuery.push(users[i]);
+        }
+      }
+      callback(null, cleanedQuery);
+    }
+    // Send results to callback function 
+  ], function(err, results) {
+    if (err) console.log(err);
+    cb(results);
+  });
+};
