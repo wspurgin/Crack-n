31b14cf6e2af0aa063cbf81a1e0e777e06e2f8cd
Nigel Gilbert <ngilbert@smu.edu>
2014-11-01 22:27:46 -0500

    deleted a bad route that accidentally made its way into activity.js routes

--- packages/custom/project/server/routes/activity.js
+++ packages/custom/project/server/routes/activity.js
@@ -6,11 +6,7 @@ module.exports = function(MeanUser, app, auth, database, passport) {
 
   app.route('/projects/:project_id/activity')
     .get(ActivityLog.getProjectActivity);
-
- // route needs to get deleted later
-   app.route('/activity/test/search')
-     .get(ActivityLog.searchUsers);
-
+    
    app.route('/activity/test')
      .post(ActivityLog.testCreateEntry);
 
