a9d6f74151d9c15bfc701e24bcb8fc09a4011dc0
Tyler George <tyler.george@live.com>
2014-12-08 14:49:01 -0600

    Finished info for my projects page

--- packages/custom/project/public/controllers/my-projects.js
+++ packages/custom/project/public/controllers/my-projects.js
@@ -9,12 +9,61 @@ angular.module('mean.project').controller('MyProjectsCtrl', ['$scope', '$rootSco
   	$scope.projects = [];
   	$scope.projectsError = null;
 
+    var handledTasks = [];
+
   	$http.get('/projects', $scope.global.user)
   	  .success(function (res) {
         $scope.projects = res;
+        calculateCompletion();
       })
       .error(function (error) {
         $scope.projectsError = error;
 
+    });
+
+    function calculateCompletion () {
+      angular.forEach($scope.projects, function (project) {
+        // console.log(project._id);
+        project.tasks = [];
+        project.totalTasks = 0;
+        project.completedTasks = 0;
+        project.completion = 0;
+        
+        angular.forEach(project.phases, function (phase) {
+          $http.get('/projects/' + project._id + '/phases/' + phase + '/tasks', $scope.global.user)
+          .success(function (res) {
+            var tempTasks = [];
+            angular.forEach(res, function (task) {
+              var taskPhase = [];
+              taskPhase.task = task;
+              taskPhase.phase = phase;
+              project.tasks.push(taskPhase);
+              tempTasks.push(taskPhase);
+            });
+
+            angular.forEach(tempTasks, function (task) {
+              if($.inArray(task.task, handledTasks) === -1) {
+                $http.get('/projects/' + project._id + '/phases/' + task.phase + '/tasks/' + task.task, $scope.global.user)
+                .success(function (res) {
+                  project.totalTasks = project.tasks.length;
+                  if(res.completed === true) 
+                    project.completedTasks += 1;
+
+                  project.completion = parseInt((project.completedTasks / project.totalTasks) * 100);
+                  // console.log('Project ' + project._id + ' completion = ' + project.completion);
+                })
+                .error(function (error) {
+                  $scope.projectsError = error;
+                });
+              }
+            });
+             
+          })
+          .error(function (error) {
+            $scope.projectsError = error;
+          });
+        });
       });
+    // console.log($scope.projects);
+    }
 }]);
